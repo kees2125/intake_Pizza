49ced0ed3ec925351bd9725a0cdb1d6a8b4bf7f4
Kees Zijlmans <kpzijlma@avans.nl>
2016-06-06 17:50:15 +0200

    UI update

--- the Pizzaria/Assets/Logic.cs
+++ the Pizzaria/Assets/Logic.cs
@@ -2,19 +2,15 @@
 using System.Collections;
 
 public class Logic : MonoBehaviour {
-    float scaleX;
-    float scaleY;
-	
+   
 	void Start () {
-        scaleX = Screen.width / 1920;
-        scaleY = Screen.height / 1080;
-        new
-
+       
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
+
+    
 }
---
+++ the Pizzaria/Assets/MenuState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuState : MonoBehaviour {
+    static private int BaseSelected { get; set; }
+    static private bool cheese { get; set; }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
